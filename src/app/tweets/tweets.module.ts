95c3918428bef18b8a9d9387fa3c126658629b1e
aman395jain <er.amanjain395@gmail.com>
2020-03-21 17:06:46 +0530

    minor changes

--- src/app/tweets/tweets.module.ts
+++ src/app/tweets/tweets.module.ts
@@ -2,20 +2,13 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { TweetsComponent } from "./tweets.component";
 import { TweetComponent } from "./tweet/tweet.component";
-import { InfiniteScrollModule } from "ngx-infinite-scroll";
 import { RouterModule } from "@angular/router";
 import { SharedModule } from "../shared/shared.module";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatCardModule, MatPaginatorModule } from "@angular/material";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    InfiniteScrollModule,
-    RouterModule,
-    SharedModule,
-    MatPaginatorModule
-  ],
+  imports: [CommonModule, RouterModule, SharedModule, MatPaginatorModule],
   declarations: [TweetsComponent, TweetComponent]
 })
 export class TweetsModule {}
