78def5401ec959a5773053b21cdf4f115e62960d
aman395jain <er.amanjain395@gmail.com>
2020-03-27 17:39:27 +0530

    spec for tweet effect

--- src/app/tweets/ngrx/tweet_effect.spec.ts
+++ src/app/tweets/ngrx/tweet_effect.spec.ts
@@ -1,9 +1,7 @@
 import { TestBed, fakeAsync, tick, async } from "@angular/core/testing";
 import { TweetEffect } from "./tweet_effect";
-import { EffectsMetadata, getEffectsMetadata } from "@ngrx/effects";
-import { Observable, of, ReplaySubject } from "rxjs";
+import { of, ReplaySubject } from "rxjs";
 import { TweetService } from "src/app/shared/tweets_service";
-import { DebugElement } from "@angular/core";
 import { provideMockActions } from "@ngrx/effects/testing";
 
 import * as tweetAction from "./tweet_action";
@@ -11,7 +9,6 @@ import * as tweetAction from "./tweet_action";
 describe("BookEffects", async () => {
   let tweetEffect: TweetEffect;
   let actions$: ReplaySubject<any> = new ReplaySubject(1);
-  let tweetService: TweetService;
   let mockTweetService: jasmine.SpyObj<TweetService>;
 
   beforeEach(() => {
@@ -43,9 +40,7 @@ describe("BookEffects", async () => {
       ]
     };
 
-    const mockTweetServiceData = mockTweetService.getTweets.and.returnValue(
-      of(mockTweets)
-    );
+    mockTweetService.getTweets.and.returnValue(of(mockTweets));
 
     actions$.next(tweetAction.fetchTweets({ payload: { value: 1 } }));
     tweetEffect.fetchNewTweets$.subscribe(tweetData => {
