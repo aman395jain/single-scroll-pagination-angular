8bcdb2cdfb825d3e10c0371d76197415d3657f8a
aman395jain <er.amanjain395@gmail.com>
2020-03-30 11:02:18 +0530

    code refactor

--- src/app/shared/tweets_service.ts
+++ src/app/shared/tweets_service.ts
@@ -1,23 +1,16 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpParams } from "@angular/common/http";
-import { Observable, BehaviorSubject } from "rxjs";
-import { map, delay, debounceTime } from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
+import { map, delay } from "rxjs/operators";
 
 @Injectable()
 export class TweetService {
-  // tweetData = {
-  //   data: {},
-  //   currentPage: 0,
-  //   hasMore: true,
-  //   perPage: 10,
-  //   total: 50,
-  //   lastPage: 5
-  // };
-  constructor(private http: HttpClient) {}
-
   getTweets(pageNumber: number) {
     return this.http
       .get("http://localhost:3000/tweets?_page=" + pageNumber)
-      .pipe(map(res => res));
+      .pipe(
+        delay(1000),
+        map(res => res)
+      );
   }
+  constructor(private http: HttpClient) {}
 }
